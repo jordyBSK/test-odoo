4d302cce339cf1071a4a0067a105683bda05cce3
dylan <dylan.bossoku@jobtrek.ch>
2024-08-06 10:05:29 +0200

    feat: add mangatheque security

--- odoo/custom/mangatheque/__manifest__.py
+++ odoo/custom/mangatheque/__manifest__.py
@@ -2,14 +2,15 @@
 {
     'name': "mangatheque",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "application de gestion des manga",
 
     'description': """
-Long description of module's purpose
+    Cette application vous permet de gérer tous vos mangas.
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "jordyBSK",
+    'website': "https://github.com/jordyBSK/",
+    'application': True,
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
